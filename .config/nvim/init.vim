863a83441c8c03ef3381f5a9a3686be5d7687ef9
Thanh <quang.thanh.ta@pr1meti.de>
2021-06-19 14:41:55 +0200

    feat: Add Doom Emacs config
    - Rename vim configs
    - Prepare for Neovim 0.5

---
+++ .config/nvim/init.vim
@@ -0,0 +1,30 @@
+syntax on
+
+set cmdheight=2
+set expandtab
+set guicursor=
+set hidden
+set incsearch
+set nobackup
+set noerrorbells
+set nohlsearch
+set noshowmatch
+set noshowmode
+set noswapfile
+set nowrap
+set nowritebackup
+set nu
+set relativenumber
+set scrolloff=8
+set shiftwidth=4
+set shortmess+=c
+set smartcase
+set smartindent
+set tabstop=4 softtabstop=4
+set termguicolors
+set undodir=~/.vim/undodir
+set undofile
+set updatetime=50
+
+set colorcolumn=80
+highlight ColorColumn ctermbg=0 guibg=lightgrey
---
+++ .doom.d/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(custom-safe-themes
+   '("6c727d2d8992257506352dae2c684406965dea08ce1135db5ba2c7ea4a9b885c" "f65451261f23f770b06f2ea96a9d349c5c7eaec15cc057873010f2f11a4c7229" "f6f616c09ceceaea46da278e29ab37d335c503080a9cd9b6f3c134c8615fbcd0" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "8fd2d543bdaa2858240dd7814bd8ee78c869f90bffed37a06316dd6407510260" "75e76b28ae727874168a7d11520def6d482946a156fd7610ce2edcddbc9fe1b5" "20f786e5d7b19abd8d93579a809976f2e477359a64ffdf36c4a07c6875045ece" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "e3713032de69725c7dd17688460b81657a086bc1d8c2ccff85ba2c98ce5b4f65" "81e22639b3ed83056deb9a12313a2937f2008e5e2dc5918c9357e29fe9a3483f" "22c8110ceeade36347426926ae4adac25067743f49354fd04d9d49c051cb5149" "34e89196734641234800804ecdb2c20505e81586b816cc906eb319c377225ff5" default))
+ '(fci-rule-color "#1c1e23")
+ '(jdee-db-active-breakpoint-face-colors (cons "#3d434d" "#ffcc66"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#3d434d" "#bae67e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#3d434d" "#171b24"))
+ '(objed-cursor-color "#f28779")
+ '(package-selected-packages '(lsp-java))
+ '(pdf-view-midnight-colors (cons "#cbccc6" "#1f2430"))
+ '(rustic-ansi-faces
+   ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(vc-annotate-background "#1f2430")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#bae67e")
+    (cons 40 "#d1e07e")
+    (cons 60 "#e8da7f")
+    (cons 80 "#ffd580")
+    (cons 100 "#ffc573")
+    (cons 120 "#ffb666")
+    (cons 140 "#ffa759")
+    (cons 160 "#f0af90")
+    (cons 180 "#e2b7c7")
+    (cons 200 "#d4bfff")
+    (cons 220 "#deacd2")
+    (cons 240 "#e899a5")
+    (cons 260 "#f28779")
+    (cons 280 "#bb6c63")
+    (cons 300 "#84514e")
+    (cons 320 "#4d3639")
+    (cons 340 "#1c1e23")
+    (cons 360 "#1c1e23")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
